56815a49d9e188b6b4a1e4e5f23631cef1fb8fee
LuvbCa <lmimathaea@gmail.com>
2023-12-27 04:24:40 +0100

    updated sveltkit to 2.0.0 and fixed minor bugs so nathene starts

---
+++ servers/core/ui_svelte/new_svelte/src/lib/Loading.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { SpinLine } from 'svelte-loading-spinners';
+
+
+</script>
+
+<div
+	class="w-full h-screen flex flex-col gap-32 justify-center items-center content-center bg-gray-900"
+>
+	<h1 class="font-bold text-white">Nathene is loading assets</h1>
+	<SpinLine size="200" color="#FFFFFF" unit="px" />
+</div>
---
+++ servers/core/ui_svelte/new_svelte/src/lib/components/Tab.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+<div class="w-1/3 bg-gray-500 border-2 border-black">Tab</div>
